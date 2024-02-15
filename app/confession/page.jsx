66ac9170dd4cd98ef2fdf1626c6b35b6f9fb7d8e
kevinsofyan <kevinsofyan.13@gmail.com>
2024-02-15 12:04:07 +0700

    test

--- app/confession/page.jsx
+++ app/confession/page.jsx
@@ -1,5 +1,6 @@
 import { Post } from "@/components/post/Post";
 import { confessionData } from "@/mock/confession";
+import Script from "next/script";
 
 export const metadata = {
   title: "Admit your wrongdoings",
@@ -33,6 +34,21 @@ const Confession = async () => {
   return (
     <main className="flex min-h-screen bg-reddit-gray">
       {postData && <Post data={postData} />}
+      <Script src="https://azara-ai-images.s3.ap-southeast-1.amazonaws.com/static/azara-bot-loader.js"></Script>
+      <azara-bot
+        widget-id="ae63691b-622c-463a-b548-bd0dd8a639c1"
+        mode="DARK"
+        tenant-id="null"
+        agent-id="c3814cfc-ebee-437d-b739-000efc66040e"
+        show-form="true"
+        show-source="false"
+        show-branding="true"
+        show-history="false"
+        show-welcome="true"
+        bg-color="#2D334D"
+        chat-color="#1B1D39"
+        show-chat-status="false"
+      ></azara-bot>
     </main>
   );
 };
